4be16a06c9d6cc9daeb829aeaeaf995dc21a49ea
Iryna Konovalova <iconovalova19@gmail.com>
2024-04-14 10:53:41 +0200

    add rendering list of courses on home page

--- src/libs/pages/HomePage/HomePage.tsx
+++ src/libs/pages/HomePage/HomePage.tsx
@@ -1,9 +1,14 @@
 import { useEffect, useState } from 'react';
-import avatar from '../../../assets/icons/avatar.svg';
+import Pagination from '@mui/material/Pagination';
+import Stack from '@mui/material/Stack';
+import { Link } from 'react-router-dom';
+
+import avatar from '../../../assets/img/avatar-tom.jpg';
 import { useAppDispatch, useAppSelector } from '../../app/hooks';
 import * as aiActions from '../../slices/aiSlice';
-import { Loader } from '../../components/Loader';
-import { IInspirationResponse } from '../../types';
+import { Loader, CourseCard } from '../../components';
+import { AppRoute, IInspirationResponse } from '../../types';
+import { COURSES } from '../../constants/courses';
 
 import style from './HomePage.module.scss';
 
@@ -76,18 +81,33 @@ export const HomePage = () => {
         )}
       </section>
 
-      <button
-        type="button"
+      <Link
+        to={AppRoute.FIND_COURSES_FORM}
         className={style.findCourseButton}
       >
         Find course
-      </button>
+      </Link>
 
       <div className={style.courses}>
         <h2 className={style.title}>Courses</h2>
 
-        <div className={style.cards}>Cards</div>
-        <div className={style.pagination}>pagination</div>
+        <div className={style.cards}>
+          {COURSES.map(course => (
+            <CourseCard
+              course={course}
+              key={course.id}
+            />
+          ))}
+        </div>
+        <div className={style.pagination}>
+          <Stack spacing={2}>
+            <Pagination
+              count={COURSES.length}
+              variant="outlined"
+              shape="rounded"
+            />
+          </Stack>
+        </div>
       </div>
     </div>
   );
